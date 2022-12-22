8171e9cb32bcf256db801475a217e36edc0bca23
Alex Via <alexviacoll@gmail.com>
2022-12-21 23:44:52 -0800

    Update vimrc

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ fi
 DST=$1
 SRC=$(realpath $(dirname "${BASH_SOURCE[0]}")) # Absolute path to dotfiles folder
 
-mkdir -p "$DST"/.config/vim
-ln -sf "$SRC"/vim/vimrc "$DST"/.config/vim/vimrc
+mkdir -p "$DST"/.vim
+ln -sf "$SRC"/vim/vimrc "$DST"/.vim/vimrc
 
 mkdir -p "$DST"/.config/i3
 ln -sf "$SRC/i3/config" "$DST"/.config/i3/config
