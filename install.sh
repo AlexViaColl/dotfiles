e188ea6d73c983acb72af0b977b10c2e05d12f09
Alex Via <alexviacoll@gmail.com>
2022-12-21 22:08:55 -0800

    Add install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]; then
+    echo 'Usage: install.sh $HOME'
+    exit 1
+fi
+
+DST=$1
+SRC=$(realpath $(dirname "${BASH_SOURCE[0]}")) # Absolute path to dotfiles folder
+
+mkdir -p "$DST"/.config/vim
+ln -sf "$SRC"/vim/vimrc "$DST"/.config/vim/vimrc
+
+mkdir -p "$DST"/.config/i3
+ln -sf "$SRC/i3/config" "$DST"/.config/i3/config
